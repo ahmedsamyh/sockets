de0adb97076b456ded81b67f9ef6919f9056db93
ahmedsamyh <ahmedsamyh10@gmail.com>
2023-09-27 18:35:49 +0500

    remove previous implementation.

--- src/client.cpp
+++ src/client.cpp
@@ -6,45 +6,6 @@
 
 int main(int argc, char *argv[]) {
   //
-  sf::TcpSocket server;
 
-  // try to connect until it gets connected
-  sf::Socket::Status status{};
-  do {
-    sf::Socket::Status status = server.connect("127.0.0.1", 8888);
-  } while (status != sf::Socket::Status::Done);
-
-  if (status != sf::Socket::Status::Done) {
-    std::cerr << "ERROR: Could not connect socket!\n";
-    return 1;
-  }
-
-  while (true) {
-    std::string data{};
-    sf::Socket::Status s{};
-    s = receive_message_from_socket(server, data);
-
-    if (s == sf::Socket::Status::Error) {
-      std::cerr << "ERROR: Could not receive data\n";
-      return 1;
-    }
-
-    if (!data.empty() && data != "Success")
-      std::cout << fmt("SERVER: {}\n", data);
-
-    std::string data_to_send{};
-    std::cin >> data_to_send;
-    if (!data_to_send.empty()) {
-      s = send_message_to_socket(server, data_to_send);
-      if (s == sf::Socket::Status::Error) {
-        std::cerr << "ERROR: Could not send data\n";
-        return 1;
-      }
-    }
-
-    if (s == sf::Socket::Status::Disconnected) {
-      std::cout << "INFO: disconnected from server!\n";
-      break;
-    }
-  }
+  return 0;
 }
