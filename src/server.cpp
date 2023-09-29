63b4d67fbdd67e5072b9d60d6728d23a190186ed
ahmedsamyh <ahmedsamyh10@gmail.com>
2023-09-30 00:30:47 +0500

    introduce name for client.

--- src/server.cpp
+++ src/server.cpp
@@ -12,6 +12,11 @@ void err() {
   exit(1);
 }
 
+struct Client {
+  std::string name;
+  socket_ptr socket;
+};
+
 int main(int argc, char *argv[]) {
   //
   std::cout << std::unitbuf;
@@ -20,7 +25,7 @@ int main(int argc, char *argv[]) {
   sf::TcpListener listener;
   sf::SocketSelector l_selector;
   sf::SocketSelector selector;
-  std::vector<socket_ptr> clients;
+  std::vector<Client> clients;
 
   std::cout << "INFO: Listening to incoming connections...\n";
   auto s = listener.listen(port);
@@ -32,67 +37,58 @@ int main(int argc, char *argv[]) {
   while (true) {
   accept:
     if (l_selector.wait(sf::seconds(0.1f))) {
-      clients.push_back(std::make_shared<sf::TcpSocket>());
-      socket_ptr client = clients.back();
-      s = listener.accept(*client);
+      clients.push_back({"NO_NAME", std::make_shared<sf::TcpSocket>()});
+      Client &client = clients.back();
+      s = listener.accept(*client.socket);
       if (s == sf::Socket::Status::Error) {
         err();
       }
 
-      std::cout << "INFO: Client " << client->getRemoteAddress().toString()
-                << ":" << client->getRemotePort() << " connected...\n";
-
-      selector.add(*client);
+      selector.add(*client.socket);
     }
 
     if (selector.wait(sf::seconds(0.1f))) {
       for (int i = 0; i < clients.size(); ++i) {
         auto &c = clients[i];
-        if (selector.isReady(*c)) {
+        if (selector.isReady(*c.socket)) {
 
           std::string receive_packet;
           receive_packet.resize(MAX_PACKET_SIZE);
           size_t received{0};
 
-          s = c->receive((void *)receive_packet.c_str(), MAX_PACKET_SIZE,
-                         received);
+          s = c.socket->receive((void *)receive_packet.c_str(), MAX_PACKET_SIZE,
+                                received);
           if (s == sf::Socket::Status::Error) {
             err();
           } else if (s == sf::Socket::Status::Disconnected) {
-            std::cout << "INFO: Client " << c->getRemoteAddress().toString()
-                      << ":" << c->getRemotePort() << " disconnected...\n";
-            selector.remove(*c);
-            c->disconnect();
+            std::cout << "INFO: Client " << c.name << " disconnected...\n";
+            selector.remove(*c.socket);
+            c.socket->disconnect();
             clients.erase(clients.begin() + i);
             goto accept;
           }
           receive_packet.resize(received);
-          std::cout
-              << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-                 "<<<<<\n";
-          std::cout << c->getRemoteAddress().toString() << ":"
-                    << c->getRemotePort() << ": " << receive_packet << "\n";
-          std::cout
-              << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-                 "<<<<<\n";
+
+          if (c.name == "NO_NAME") {
+            c.name = receive_packet;
+            std::cout << "INFO: " << c.name << " connected...\n";
+          } else {
+            // send message to other clients
+            for (auto &other : clients) {
+              if (other.name != c.name) {
+                std::string send_packet = receive_packet;
+                s = other.socket->send(send_packet.c_str(), send_packet.size());
+                if (s == sf::Socket::Status::Error) {
+                  err();
+                }
+              }
+            }
+
+            std::cout << c.name << ": " << receive_packet << "\n";
+          }
         }
       }
     }
-    // std::string send_packet{};
-    // if (receive_packet == "ACK REQ") {
-    //   send_packet = "ACK CONFIRM";
-    // } else if (receive_packet == "ACK ACK") {
-    // }
-
-    // if (!send_packet.empty()) {
-    //   std::cout << "INFO: Sending packet to client "
-    //             << client->getRemoteAddress().toString() << ":"
-    //             << client->getRemotePort() << "...\n";
-    //   s = client->send(send_packet.c_str(), send_packet.size());
-    //   if (s == sf::Socket::Status::Error) {
-    //     err();
-    //   }
-    // }
   }
 
   return 0;
